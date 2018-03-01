62d84a892df77f5b090b0d99fcc48bbecee37bc2
zinukov <36642204+zinukov@users.noreply.github.com>
2018-03-01 20:48:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var numbers = equation.match(/(-?|\+\s|)[^\^][0-9]+/g).map(function(item) {
+    return Number(item.replace(/\s/g, ''));
+  });
+  var [a,b,c] = numbers;
+  
+  var D = Math.pow(b,2) - 4 * a *c;
+  var first = (-b + Math.sqrt(D)) / (2 * a);
+  var second = (-b - Math.sqrt(D)) / (2 * a);
+  
+  var result = [Math.round(first), Math.round(second)];
+  return result.sort(function(a, b) {
+    return a -b;
+  });
 }
